ecd8df73902039e77e896d2888374d742a9d84ba
Alejo Funes <alefunesrios@gmail.com>
2025-05-06 21:33:50 -0300

    agrego cors y empiezo a hacer uri_front

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import 'dotenv/config'
+import path from 'node:path'
+import cors from 'cors'
 import connection  from './utils/connection.js'
 import routerProductos from './routers/productos.router.js'
 import routerUsuarios from './routers/usuarios.router.js'
@@ -10,10 +12,16 @@ import routerUploads from './routers/uploads.router.js'
 const app = express()
 const PORT = 8080
 const URI_DB = process.env.URI_LOCAL
+const URI_FRONT = process.env.URI_FRONTEND_CORS
 
+const corsConfig = {
+    origin: URI_FRONT     //'front-end' // acá hay que poner la ruta del front
+}
 
+// ! MIDDLEWARES
 app.use(express.json()) // hace que funcione el bddy dl postman
-
+app.use(express.static(path.join('public'))) // algo que ver con / y \
+app.use(cors(corsConfig))
 // ! RUTAS
 app.use('/api/v1/productos', routerProductos) // aca adentro sucede la magia
 app.use('/api/v1/usuarios', routerUsuarios) // aca adentro sucede la magia
