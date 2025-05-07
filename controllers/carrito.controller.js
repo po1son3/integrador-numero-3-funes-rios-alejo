3610bf1ec77874ae785f5d1fd9976b8d3977c3ce
Alejo Funes <alefunesrios@gmail.com>
2025-05-07 20:16:46 -0300

    actualizo el carrito.controller

--- controllers/carrito.controller.js
+++ controllers/carrito.controller.js
@@ -1,11 +1,16 @@
+import model from '../models/carrito.models.js'
+
 
 const guardarCarrito = async (req, res) =>  {
     const carrito = req.body
 
     try {
-        
+        const carritoGuardado = await model.crearCarrito(carrito)
+        res.status(201).json(carritoGuardado)
     } catch (error) {
-        
+        console.log('[guardarCarrito]', error)
+        let mensaje = 'No se pudo guardar el carrito'
+        res.status(500).json(mensaje)
     }
 
 }
