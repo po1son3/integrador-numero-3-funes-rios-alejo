895c5875a50719d459fc83256d987f84969ab861
Alejo Funes <alefunesrios@gmail.com>
2025-05-07 20:10:53 -0300

    actualizo carrito models

--- models/carrito.models.js
+++ models/carrito.models.js
@@ -1,7 +1,24 @@
 import mongoose from "mongoose";
 
+
+// schema
 const carritoSchema = mongoose.Schema({
     carrito: Array
 })
+// model
+const CarritoModel = mongoose.model('carritos', carritoSchema)
 
-const carritoModel = mongoose.model('carritos', carritoSchema)
+const crearCarrito = async (carrito) => {
+
+    try {
+        const carritoCreado = new CarritoModel( { carrito } )
+        const carritoGuardado = await carritoCreado.save()
+        return carritoGuardado
+    } catch (error) {
+        throw error
+    }
+
+}
+export default {
+    crearCarrito
+}
